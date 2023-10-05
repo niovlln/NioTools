65f0a24979a8fc405a3da0c6eb2c1f1578423361
niovlln <136703189+niovlln@users.noreply.github.com>
2023-10-05 12:08:55 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -27,19 +27,28 @@ function addPoints() {
 
 
 //USERNAME LIST
-const usernames = [];
+let usernames = [];
 displayUsernames();
+loadUsernamesFromLocalStorage();
 
 function displayUsernames() {
     document.getElementById("usernames").innerHTML = usernames.join("<br>");
 }
 
+function loadUsernamesFromLocalStorage() {
+    const savedUsernames = localStorage.getItem("savedUsernames");
+    if (savedUsernames) {
+        usernames = JSON.parse(savedUsernames);
+        displayUsernames();
+    }
+}
 
 //ADD USERNAME
 function addUsername() {
     const input = document.getElementById("demo").value.trim();
     if (input) {
         usernames.push(input);
+        saveUsernamesToLocalStorage()
         displayUsernames();
         document.getElementById("demo").value = "";
     }
@@ -48,6 +57,28 @@ function addUsername() {
 //SORT USERNAME
 function sortUsername() {
     usernames.sort();
+    displayUsernames();
+}
+
+//RANDOM USERNAME PICK 
+function pickRandomUsername() {
+    if (usernames.length === 0 ) {
+        alert("No Username Available");
+    }
+    else{
+        const randomIndexUsername = Math.floor(Math.random() * usernames.length);
+    alert(usernames[randomIndexUsername]);
+    }
+    
+}
+//SAVE USERNAMES TO LOCAL STORAGE
+function saveUsernamesToLocalStorage() {
+    localStorage.setItem("savedUsernames", JSON.stringify(usernames));
+}
+//REMOVE USERNAMES FROM LOCAL STORAGE
+function removeUsernamesFromLocalStorage() {
+    usernames = [];
+    localStorage.removeItem("savedUsernames");
     displayUsernames();
 }
 
